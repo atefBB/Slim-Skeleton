77911affd3d08bdf030c8dc64bb8d54677e3d45b
Atef Ben Ali <atef.bettaib@gmail.com>
2019-08-01 10:59:41 +0200

    style: format `~/public/index.php` file

--- public/index.php
+++ public/index.php
@@ -6,7 +6,7 @@ $app = new \Slim\Slim(array(
     'templates.path' => '../templates',
 ));
 
-// Create monolog logger and store logger in container as singleton 
+// Create monolog logger and store logger in container as singleton
 // (Singleton resources retrieve the same log resource definition each time)
 $app->container->singleton('log', function () {
     $log = new \Monolog\Logger('slim-skeleton');
@@ -17,12 +17,13 @@ $app->container->singleton('log', function () {
 // Prepare view
 $app->view(new \Slim\Views\Twig());
 $app->view->parserOptions = array(
-    'charset' => 'utf-8',
-    'cache' => realpath('../templates/cache'),
-    'auto_reload' => true,
+    'charset'          => 'utf-8',
+    'cache'            => realpath('../templates/cache'),
+    'auto_reload'      => true,
     'strict_variables' => false,
-    'autoescape' => true
+    'autoescape'       => true,
 );
+
 $app->view->parserExtensions = array(new \Slim\Views\TwigExtension());
 
 // Define routes
